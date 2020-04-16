1c1735bccb97b5bbeb6b4e9f340c6e57ed3bcb83
chonghorizons <chonghorizons@gmail.com>
2020-04-15 20:37:35 -0400

    simulations in new file

---
+++ simulations.js
@@ -0,0 +1,89 @@
+console.log(PopulationInitial);
+
+GLOBALquarantineCompliance=0.0;
+PodQuarantine=false;
+ContactTraceBack=false;
+ContactTraceForward=false;
+var Storage1= [];
+InitializeEverything0({
+				  	atRisk: false,
+					exposuresPerWeek: 80,
+					distancingCompliance: 0.5, 
+					quarantineCompliance: 0.0,
+					podIntegrity: 0.0,
+					noHandwash: 1.0,
+				});
+// Run 30 rounds
+for (var i=0; i<30; i++) {
+	t(Storage1);
+}
+
+// sample person
+PodArray[0][0];
+var Run1PodArray=PodArray;
+
+
+//////////
+//////////
+//////// #2
+var Storage2= [];
+InitializeEverything0({
+				  	atRisk: false,
+					exposuresPerWeek: 20,
+					distancingCompliance: 0.5, 
+					quarantineCompliance: 0.0,
+					podIntegrity: 1.0,
+					noHandwash: 1.0,
+				});
+// Run 20 rounds
+for (var i=0; i<20; i++) {
+	t(Storage2);
+}
+
+// sample person
+PodArray[0][0];
+var Run2PodArray=PodArray;
+
+
+
+var Storage3= [];
+InitializeEverything0({
+				  	atRisk: false,
+					exposuresPerWeek: 20,
+					distancingCompliance: 0.5, 
+					quarantineCompliance: 0.0,
+					podIntegrity: 0.8,
+					noHandwash: 1.0,
+				});
+// Run 20 rounds
+for (var i=0; i<20; i++) {
+	t(Storage3);
+}
+
+// sample person
+PodArray[0][0];
+var Run3PodArray=PodArray;
+
+
+GLOBALquarantineCompliance=1.0;
+PodQuarantine=true;
+ContactTraceBack=true;
+ContactTraceForward=true;
+var Storage4= [];
+InitializeEverything0({
+				  	atRisk: false,
+					exposuresPerWeek: 20,
+					distancingCompliance: 0.5, 
+					quarantineCompliance: 1.0,
+					podIntegrity: 0.8,
+					noHandwash: 1.0,
+				});
+// Run 20 rounds
+for (var i=0; i<20; i++) {
+	t(Storage4);
+}
+
+// sample person
+PodArray[0][0];
+var Run4PodArray=PodArray;
+
